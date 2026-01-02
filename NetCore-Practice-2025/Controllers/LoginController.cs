e1a34f371ce95c6b781c1ac7ee606ceefbf52c44
shibuthomas-2022mt93003 <2022mt93003@wilp.bits-pilani.ac.in>
2026-01-03 04:11:20 +0530

    updated code to learn web api's

---
+++ NetCore-Practice-2025/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore_Practice_2025.Controllers
+{
+    [Route("api/login")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        // contructor
+        public LoginController() { 
+        
+        
+        }
+
+
+        [HttpGet("login")]
+        public ActionResult ReturnUsername() {
+
+
+            return Ok(new
+            {
+                success = true,
+                message = "Fetched successfully",
+                data = new { id = 1, name = "Shibu" },
+                timestamp = DateTime.UtcNow
+            });
+
+        }
+    }
+}
--- NetCore-Practice-2025/Program.cs
+++ NetCore-Practice-2025/Program.cs
@@ -1,26 +1,33 @@
 using NetCore_Practice_2025.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// add controllers
+builder.Services.AddControllers();
+
 // adding custom middleware
-builder.Services.AddTransient<CustomMiddleware>();
+//builder.Services.AddTransient<CustomMiddleware>();
 var app = builder.Build();
 
-app.Use( async ( HttpContext context, RequestDelegate next ) =>
-{
-    await context.Response.WriteAsync("hello");
-    await next(context);
-});
-
+//app.Use( async ( HttpContext context, RequestDelegate next ) =>
+//{
+//    await context.Response.WriteAsync("hello");
+//    await next(context);
+//});
 
 //app.UseMiddleware<CustomMiddleware>();
-
 // using extension methods
-app.UseCustomMiddleware();
+//app.UseCustomMiddleware();
 
-app.Run(async (HttpContext context) =>
-{
-    await context.Response.WriteAsync("hello world");
-});
+app.MapControllers();
+
+//app.UseRouting();
+
+//app.Run(async (HttpContext context) =>
+//{
+//    await context.Response.WriteAsync("hello world");
+//});
+
 
 app.Run();
 
